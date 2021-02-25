d0551bf1a955bdb65cc367a9527e136174e964e7
Carol Marulanda <clmarulandac@gmail.com>
2021-02-25 08:58:29 -0500

    Modificacion HTML CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
 console.log(validator);
+//Almacenar el numero de la tarjeta ingresado en el input Numero de tarjeta
+const numeroTarjeta = document.getElementById('numCard');
+numeroTarjeta.addEventListener('keyup',evaluarSoloNumeros);
+
+function evaluarSoloNumeros(){
+const valorNumeroTarjeta = numeroTarjeta.value;
+const result = valorNumeroTarjeta.replace(/\D/g, "");
+document.getElementById("numCard").value = result;
+
+console.log(valorNumeroTarjeta);
+
+}
