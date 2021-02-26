d27f0f5c6e048a0a4fd5d270e1e8d3b19dd0a804
Ruby <alvarezruby098@gmail.com>
2021-02-26 11:26:31 -0500

    nuevas funciones

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,22 @@
-const validator = {
-  // ...
+const validator = { // Es un objeto con el cual podemos agrupar funciones
+  isValid
+  maskify
+  
 };
+ 
+export default validator; // export por default significa que se exporta automaticamente 
 
-export default validator;
+
+function isValid(creditCardNumber){
+    // aqui va el codigo con las operaciones para verificar si la tarjeta es valida
+  if(variable == 0){
+    return true;
+  }
+  else 
+  return false;
+}
+
+function maskify(creditCardNumber){
+  // aqui va el codigo que oculta los digitos menos los cuatro ultimos
+  return ######1234;
+}
