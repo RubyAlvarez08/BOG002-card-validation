8796f14a38ddbaab26afb8145a0a115b02941021
Carol Marulanda <clmarulandac@gmail.com>
2021-02-26 11:41:05 -0500

    HTML JS

--- src/index.js
+++ src/index.js
@@ -10,6 +10,33 @@ const valorNumeroTarjeta = numeroTarjeta.value;
 const result = valorNumeroTarjeta.replace(/\D/g, "");
 document.getElementById("numCard").value = result;
 
-console.log(valorNumeroTarjeta);
-
 }
+
+ function array(evaluarSoloNumeros){
+    const convertirAUnArray = result.split("").reverse();
+
+    console.log (convertirAUnArray);
+ }
+
+// Evalua si el campo esta vacio y si lo esta arroja una alerta 
+
+function campoVacio(){
+    const inputTarjeta = document.getElementById('numCard').value;
+     if(inputTarjeta.length == 0){
+       alert("Este campo esta vacio");
+       return false;
+     }
+     
+  }
+  const boton = document.getElementById('button');
+button.addEventListener('click',campoVacio);
+
+/*
+//convertir cadena a un array
+
+  const cadena = evaluarSoloNumeros();
+  const convertirAUnArray = cadena.split("").reverse();
+
+  console.log(cadena);
+}
+*/
