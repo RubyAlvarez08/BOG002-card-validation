e5fb5bf8d59cfbdd2810c7c65b4829d6c7e7fd48
Carol Marulanda <clmarulandac@gmail.com>
2021-03-03 09:04:26 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ validator.js
  */
 import validator from './validator.js';
 
-
-
-
-     
-    
-<<<<<<< HEAD
-      const submitNumCard = document.getElementById('button');
-      submitNumCard.addEventListener('click',getInput);
-      
-function getInput(){ // El formulario no se envia si no cumple con las sgtes condiciones
-    const creditCardNumber = document.getElementById('numCard').value;
-
-    if(creditCardNumber == ""){ // aqui evaluamos si el campo esta vacio
-=======
     
      const submitNumCard = document.getElementById('button');
       submitNumCard.addEventListener('click',getInput);
@@ -26,24 +12,10 @@ function getInput(){ // El formulario no se envia si no cumple con las sgtes con
       const creditCardNumber = document.getElementById('numCard').value;
 
       if(creditCardNumber == ""){ // aqui evaluamos si el campo esta vacio
->>>>>>> f3a5a6dd5a0c4c7924f2f83fd48194d802c1e8bd
         alert('Este campo es obligatorio');
     
     }
-<<<<<<< HEAD
-    else if((creditCardNumber.length > 16)||(/\D/.test(creditCardNumber))){ // aqui validamos el rango de digitos ingresados
-        alert('Verifique los datos ingresados');
-        return false;
-    }
-    else 
-    return true;
-   
 
-}
-       
-
-
-=======
       else if((creditCardNumber.length > 16) || (/\D/.test(creditCardNumber))){ // aqui validamos el rango de digitos ingresados y si los digitos ingresados son diferentes a numeros 
         alert('Verifique la información ingresada');
         
@@ -53,7 +25,7 @@ function getInput(){ // El formulario no se envia si no cumple con las sgtes con
   
 }
 
->>>>>>> f3a5a6dd5a0c4c7924f2f83fd48194d802c1e8bd
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -43,36 +43,3 @@ const validator = { // Es un objeto con el cual podemos agrupar funciones
  
 export default validator; // export por default significa que se exporta automaticamente 
    
-
-
- 
-
-// aqui va el codigo con las operaciones para verificar si la tarjeta es valida
-function isValid(creditCardNumber){
-  const arrayCreditcardNumber = creditCardNumber.split(" ").reverse();
-    const suma =0;
-  for (const i = 0, i < arrayCreditcardNumber.length; i++);
-  
-  if(i %2 == 0 ){
-      const posicionesPares = arrayCreditcardNumber [i]*2;
-       if(posicionesPares >= 10){
-         posicionesPares = (posicionesPares-10)+1;
-       }
-        suma += posicionesPares;
-       else{
-
-<<<<<<< HEAD
-       }
-    return true;
-  }
-  else 
-  return false;
-}
-
-function maskify(creditCardNumber){
-  // aqui va el codigo que oculta los digitos menos los cuatro ultimos
-  return ######1234;
-}
-*/
-=======
->>>>>>> f3a5a6dd5a0c4c7924f2f83fd48194d802c1e8bd
