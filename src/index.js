b7cc996dc4e78bab57b2f452f0d106b3405afe27
Ruby <alvarezruby098@gmail.com>
2021-03-09 08:28:25 -0500

    Pagina de inicio

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-/*con esta importacion de modulos lo que se esta haciendo
-es llamar el objeto validator que se encuentra en el archivo
-validator.js
- */
+
 import validator from './validator.js';
 
-// validator.isValid();
-// validator.maskify();
-  
-const submitNumCard = document.getElementById('button');
+// Abrir ventana modal
+const cerrar = document.querySelectorAll('.close')[0];
+const abrir = document.getElementById('agregar-tarjeta');
+const modalPadre = document.querySelectorAll('.modal-isValid')[0];
+const modalHijo = document.querySelectorAll('.valid-card')[0];
 
- 
-submitNumCard.addEventListener('click',getInput);
-;
+// Abre la
+abrir.addEventListener('click', function(){
+
+modalPadre.style.opacity = "1";
+modalPadre.style.visibility = "visible";
+modalHijo.classList.toggle('modal-close');
 
 
+// Evaluar tarjeta
+const recibirNumero = document.getElementById('button');
+recibirNumero.addEventListener('click',getInput);
 
-function getInput(){ // El formulario no se envia si cumple con las sgtes condiciones
+function getInput(){ 
     
   const creditCardNumber = document.getElementById('numCard').value;
-  
-  //FILTRO DE TIIPO TEXTO
 
   //FILTROS PARA NUMERO DE TARJETA
-  if(creditCardNumber == ""){ // aqui evaluamos si el campo esta vacio
-    document.getElementById("tarjeta").textContent= "Este campo es obligatorio";
-    // return -> romper el codigo
+  if(creditCardNumber == ""){ 
+     document.getElementById("tarjeta").textContent= "Este campo es obligatorio";
     return
   }
 
   if((creditCardNumber.length > 16) || (/\D/.test(creditCardNumber))  ){ // aqui validamos el rango de digitos ingresados y si los digitos ingresados son diferentes a numeros 
     document.getElementById("tarjeta").textContent= "Verifique los valores ingresados";
-    
     return 
   }
 
 
   
-  submitNumCard.addEventListener('click',notValid());
+  recibirNumero.addEventListener('click',notValid());
 
   function notValid(){
     if(validator.isValid(creditCardNumber) == false){
@@ -46,25 +46,39 @@ function getInput(){ // El formulario no se envia si cumple con las sgtes condic
   }
  
 
-  submitNumCard.addEventListener('click',siValid());
+  recibirNumero.addEventListener('click',siValid());
 
   function siValid(){
     if(validator.isValid(creditCardNumber) == true){
-      document.getElementById("isValid").textContent = validator.maskify(creditCardNumber);
+      
+      const valida = document.getElementById("isValid");
+      valida.innerHTML = validator.maskify(creditCardNumber);
+      valida.style.background = "red";
+      valida.style.width = "40%";
+      valida.style.height ="50%";
+      valida.style.position = "center";
+      
       }
 
   }
 
   
-
-
 }  
 
 
+});
+
+cerrar.addEventListener('click', function() {
+modalHijo.classList.toggle('modal-close');
+modalPadre.style.opacity = "0";
+modalPadre.style.visibility = "hidden";
+});
+
+  
 
 
 
- 
+
 
 
 
